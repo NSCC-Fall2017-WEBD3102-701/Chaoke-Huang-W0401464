014c68daa884ed560215aaffcaa514c17810a15c
chaoke huang <w0401464@nscc.ca>
2017-11-14 20:27:29 -0400

    done

---
+++ Labs/Lab6/InvoiceFrontEnd/src/main/java/com/webd3102/Invoice.java
@@ -0,0 +1,84 @@
+package com.webd3102;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.List;
+
+
+@ManagedBean(name = "invoice")
+public class Invoice implements Comparable<Invoice>{
+    private String name;
+    private Integer id;
+    private String[] items;
+    private String orderDate;
+    private String dueDate;
+
+    public Invoice() { }
+    public Invoice(Integer id, String name, String orderDate, String dueDate, String[] items) {
+        this.id = id;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.dueDate = dueDate;
+        this.items = items;
+    }
+
+    public Invoice(String name, String orderDate, String dueDate, String[] items) {
+        this.name = name;
+        this.orderDate = orderDate;
+        this.dueDate = dueDate;
+        this.items = items;
+    }
+
+    public String[] getItems() {
+        return items;
+    }
+
+    public void setItems(String[] items) {
+        this.items = items;
+    }
+
+    public int getItemsLength() {
+        if (this.items != null) {
+            return this.items.length;
+        } else {
+            return 0;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public int compareTo(Invoice o) {
+        return this.getId().compareTo(o.getId());
+    }
+}
---
+++ Labs/Lab6/InvoiceFrontEnd/src/main/java/com/webd3102/ItemDBUtil.java
@@ -0,0 +1,104 @@
+package com.webd3102;
+
+import com.sun.deploy.util.StringUtils;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ItemDBUtil {
+
+    private static ItemDBUtil instance;
+    private DataSource dataSource;
+    private String jndiName = "java:comp/env/jdbc/Invoices";
+
+    private ItemDBUtil() throws Exception {
+        dataSource = getDataSource();
+    }
+
+    public static ItemDBUtil getInstance() throws Exception {
+        if (instance == null) {
+            instance = new ItemDBUtil();
+        }
+
+        return instance;
+    }
+
+    private DataSource getDataSource() throws NamingException {
+        Context context = new InitialContext();
+
+        DataSource theDataSource = (DataSource) context.lookup(jndiName);
+
+        return theDataSource;
+    }
+
+    public List<String> getItems() throws Exception {
+
+        List<String> items = new ArrayList<>();
+
+        Connection myConn = null;
+        Statement myStmt = null;
+        ResultSet myRs = null;
+
+        try {
+            myConn = getConnection();
+
+            String sql = "select * from Items order by Item_id";
+
+            myStmt = myConn.createStatement();
+
+            myRs = myStmt.executeQuery(sql);
+
+            // process result set
+            while (myRs.next()) {
+
+
+                items.add(myRs.getString("Item_name"));
+            }
+
+            return items;
+        } finally {
+            close(myConn, myStmt, myRs);
+        }
+    }
+
+
+    private Connection getConnection() throws Exception {
+
+        Connection theConn = dataSource.getConnection();
+
+        return theConn;
+    }
+
+    private void close(Connection theConn, Statement theStmt) {
+        close(theConn, theStmt, null);
+    }
+
+    private void close(Connection theConn, Statement theStmt, ResultSet theRs) {
+
+        try {
+            if (theRs != null) {
+                theRs.close();
+            }
+
+            if (theStmt != null) {
+                theStmt.close();
+            }
+
+            if (theConn != null) {
+                theConn.close();
+            }
+
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+}
